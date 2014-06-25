9393239163493a8f2e924eded12fb3269ef34d72
Exikle <dixondcunha@hotmail.com>
2014-06-24 23:54:04 -0400

    Created 2 new validators:
    LengthCheckValidator makes sure text length is bettween a certain range
    
    TextMatchValidator makes sure the text matches dev specified text.
    
    Signed-off-by: Exikle <dixondcunha@hotmail.com>

---
+++ library/src/com/throrinstudio/android/common/libs/validator/validator/TextMatchValidator.java
@@ -0,0 +1,47 @@
+package com.throrinstudio.android.common.libs.validator.validator;
+
+import android.content.Context;
+
+import com.throrinstudio.android.common.libs.validator.AbstractValidator;
+import com.throrinstudio.android.common.libs.validator.R;
+
+/**
+ * Checks if the text in the editText matches another text.
+ * 
+ * @author Dixon D'Cunha (Exikle)
+ */
+public class TextMatchValidator extends AbstractValidator {
+
+	/**
+	 * Default error message if none specified.
+	 */
+	private static final int DEFAULT_ERROR_MESSAGE_RESOURCE = R.string.validator_len;
+
+	/**
+	 * String that the text must match
+	 */
+	private String toMatch;
+
+	public TextMatchValidator(Context c, String str) {
+		super(c, DEFAULT_ERROR_MESSAGE_RESOURCE);
+		toMatch = str;
+	}
+
+	public TextMatchValidator(Context c, int errorMessageRes,
+			String str) {
+		super(c, errorMessageRes);
+		toMatch = str;
+	}
+
+	public TextMatchValidator(Context c, String errorMessage,
+			String str) {
+		super(c, errorMessage);
+		toMatch = str;
+	}
+
+	@Override
+	public boolean isValid(String value) {
+		return toMatch.equals(value);
+	}
+
+}
